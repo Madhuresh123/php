d9114054ce8894940d0428c2ed0ec250bf031c89
Madhuresh123 <madhuresh91394@gmail.com>
2023-07-19 16:25:43 +0530

    php destructor

---
+++ oop_destructor.php
@@ -0,0 +1,22 @@
+<?php
+
+    class Fruit{
+
+        public $name;
+        public $color;
+
+        function __construct($name, $color){
+            $this->name = $name;
+            $this->color = $color;
+        }
+
+        function __destruct(){
+            echo "The name of the fruit is $this->name and its color is $this->color";
+        }
+    }
+
+    $apple = new Fruit("Apple", "Red");
+
+
+
+?>
