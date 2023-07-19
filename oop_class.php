c248a6801571e09089bac12fb067cb0cdebfec5a
Madhuresh123 <madhuresh91394@gmail.com>
2023-07-19 15:26:08 +0530

    php class

---
+++ oop_class.php
@@ -0,0 +1,38 @@
+
+<?php
+
+
+    class Fruit{
+
+        public $name;
+        public $color;
+
+        function set_name($n) {
+            $this->name = $n;
+        }
+
+        function get_name(){
+            return $this->name;
+        }
+
+        function set_color($n){
+            $this->color = $n;
+        }
+
+        function get_color(){
+            return $this->color;
+        }
+
+    }
+
+    $apple = New Fruit();
+
+    $apple->set_name("Apples");
+    $apple->set_color("Green");
+
+    echo $apple->get_name();
+    echo "<br>";
+    echo $apple->get_color();
+
+
+?>
